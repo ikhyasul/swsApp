db36b2459b8c3c2896df30a615ffff32abf1fe36
Ironedman <dwikyhp@gmail.com>
2023-05-02 15:39:46 +0700

    Cek Over

--- app/src/main/java/com/example/myapplication/OverviewActivity.java
+++ app/src/main/java/com/example/myapplication/OverviewActivity.java
@@ -2,7 +2,10 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class OverviewActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class OverviewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_overview);
+        LinearLayout btnSetting = (LinearLayout) findViewById(R.id.btnOverSeting);
+        LinearLayout btnTutorial = (LinearLayout) findViewById(R.id.btnOverTutorial);
+        btnSetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i= new Intent(getApplicationContext(),SetingpinActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
